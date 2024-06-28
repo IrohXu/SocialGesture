5e44dd6603dfa9b51a3abf35d900ec4f809c058f
exx <sangmin-git@users.noreply.github.com>
2024-06-28 02:39:11 -0400

    make detection faster

--- tools/bbox_generation.py
+++ tools/bbox_generation.py
@@ -12,8 +12,9 @@ import json
 
 
 def yolov10_inference(video, video_id, out_path, image_size, conf_threshold):
-    model = ort.InferenceSession('/home/xucao2/checkpoints/yolov10b.onnx')
-    
+    # model = ort.InferenceSession('/home/xucao2/checkpoints/yolov10b.onnx')
+    model = YOLOv10.from_pretrained('jameslahm/yolov10x')
+
     output_json = {
         "video_id": video_id,
         "width": 640,
@@ -36,28 +37,33 @@ def yolov10_inference(video, video_id, out_path, image_size, conf_threshold):
         ret, frame = cap.read()
         if not ret:
             break
-        
-        image = cv2.resize(frame, (640, 640))
-        input_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        # Assuming the model expects a certain input size
-        input_tensor = input_image.astype(np.float32) / 255.0
-        input_tensor = input_tensor.transpose(2, 0, 1)  # Change to channel-first format
-        input_tensor = np.expand_dims(input_tensor, axis=0)
+        input_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        outputs = model.predict(input_image, verbose=False)
+        output = outputs[0]
+        boxes = output.boxes
+        boxes_person = boxes.xyxy[boxes.cls==0]
 
-        # Run the model
-        inputs = {model.get_inputs()[0].name: input_tensor}
-        outputs = model.run(None, inputs)
+        # image = cv2.resize(frame, (640, 640))
+        # input_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        # Process the output
-        output = outputs[0]  # Assuming the output is [1, 300, 6]
+        # # Assuming the model expects a certain input size
+        # input_tensor = input_image.astype(np.float32) / 255.0
+        # input_tensor = input_tensor.transpose(2, 0, 1)  # Change to channel-first format
+        # input_tensor = np.expand_dims(input_tensor, axis=0)
 
-        output = output[output[:,:,4]>conf_threshold]
-        output = output[output[:,5]==0]
-
-        # Extract bounding boxes and probabilities
-        boxes = output[:, :4]
-        probabilities = output[:, 4]
+        # # Run the model
+        # inputs = {model.get_inputs()[0].name: input_tensor}
+        # outputs = model.run(None, inputs)
+        #
+        # # Process the output
+        # output = outputs[0]  # Assuming the output is [1, 300, 6]
+        # output = output[output[:,:,4]>conf_threshold]
+        # output = output[output[:,5]==0]
+        #
+        # # Extract bounding boxes and probabilities
+        # boxes = output[:, :4]
+        # probabilities = output[:, 4]
         
         frame_info = {
             "frame": frame_id,
@@ -68,12 +74,12 @@ def yolov10_inference(video, video_id, out_path, image_size, conf_threshold):
         # max_prob_index = np.argmax(probabilities)
         # x1, y1, x2, y2 = boxes[max_prob_index]
         
-        for box in boxes:
+        for box in boxes_person:
             x1, y1, x2, y2 = box
             frame_info["segments_info"].append({
                 "category_id": 0,
                 "bbox": [
-                    int(x1), int(y1 * 360 /640), int(x2), int(y2 * 360 /640)
+                    int(x1), int(y1), int(x2), int(y2)
                 ]
             })
         
@@ -93,15 +99,17 @@ def yolov10_inference(video, video_id, out_path, image_size, conf_threshold):
         # results = model.predict(source=frame, imgsz=image_size, conf=conf_threshold)
         # annotated_frame = results[0].plot()
         # out.write(annotated_frame)
-        with open(os.path.join(out_path, video_id + '.json'), 'w') as f:
-            json.dump(output_json, f, indent=4)  # prettier json
+        # with open(os.path.join(out_path, video_id + '.json'), 'w') as f:
+        #     json.dump(output_json, f, indent=4)  # prettier json
 
     cap.release()
+    with open(os.path.join(out_path, video_id + '.json'), 'w') as f:
+        json.dump(output_json, f, indent=4)  # prettier json
 
     return None
 
 image_size = 640
-conf_threshold = 0.5
+conf_threshold = 0.6
 
 for video_name in os.listdir("/home/xucao2/SocialGesture/data_sources/pilot_dataset/videos"):
     video = os.path.join("/home/xucao2/SocialGesture/data_sources/pilot_dataset/videos", video_name)
